c61b19313e1b542dd01c9b52bc5bffe07163d269
Rogério Silva <ress.rogerio@gmail.com>
2016-11-28 23:56:42 -0200

    Adicionado módulo mysql

--- site_noticias/app/routes/noticias.js
+++ site_noticias/app/routes/noticias.js
@@ -1,10 +1,16 @@
+var dbConnection = require("../../config/dbConnection");
+
 module.exports = function(app) {
+  var connection = dbConnection();
+
   app.get('/cadastrar-noticia', function(req, res){
     res.render('admin/form_add_noticia');
   });
 
   app.get('/noticias', function(req, res){
-    res.render('noticias/noticias');
+    connection.query("select * from noticias", function(error, result){
+      res.render('noticias/noticias', {noticias : result});
+    });
   });
 
   app.get('/noticia', function(req, res){
