ba1d765c9c875dff2d3d96d32d63823ca826b625
Rogério Silva <ress.rogerio@gmail.com>
2016-12-01 22:49:04 -0200

    Refatorando a aplicação para uso de classe

--- site_noticias/app/models/noticiasModel.js
+++ site_noticias/app/models/noticiasModel.js
@@ -1,16 +1,19 @@
+function Noticias(connection) {
+  this._connection = connection;
+}
+
+Noticias.prototype.getNoticias = function(callback) {
+  this._connection.query("select * from noticias", callback);
+}
+
+Noticias.prototype.getNoticia = function(callback) {
+  this._connection.query("select * from noticias where id = 1", callback);
+}
+
+Noticias.prototype.salvarNoticia = function(noticia, callback) {
+  this._connection.query("insert into noticias set ?", noticia, callback);
+}
+
 module.exports = function() {
-
-  this.getNoticias = function(connection, callback) {
-    connection.query("select * from noticias", callback);
-  }
-
-  this.getNoticia = function(connection, callback) {
-    connection.query("select * from noticias where id = 1", callback);
-  }
-
-  this.salvarNoticia = function(noticia, connection, callback) {
-    connection.query("insert into noticias set ?", noticia, callback);
-  }
-
-  return this;
+  return Noticias;
 }
--- site_noticias/app/routes/noticias.js
+++ site_noticias/app/routes/noticias.js
@@ -8,26 +8,26 @@ module.exports = function(app) {
 
   app.get('/noticias', function(req, res){
     var connection = app.config.dbConnection();
-    var noticiasModel = app.app.models.noticiasModel;
-    noticiasModel.getNoticias(connection, function(error, result){
+    var noticiasModel = new app.app.models.noticiasModel(connection);
+    noticiasModel.getNoticias(function(error, result){
       res.render('noticias/noticias', {noticias : result});
     });
   });
 
   app.get('/noticia', function(req, res){
     var connection = app.config.dbConnection();
-    var noticiasModel = app.app.models.noticiasModel;
-    noticiasModel.getNoticia(connection, function(error, result){
+    var noticiasModel = new app.app.models.noticiasModel(connection);
+    noticiasModel.getNoticia(function(error, result){
       res.render('noticias/noticia', {noticia : result});
     });
   });
 
   app.post('/noticias/salvar', function(req, res){
     var connection = app.config.dbConnection();
-    var noticiasModel = app.app.models.noticiasModel;
+    var noticiasModel = new app.app.models.noticiasModel(connection);
 
     var noticia = req.body;
-    noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+    noticiasModel.salvarNoticia(noticia, function(error, result){
       res.redirect("/noticias");
     });
   });
