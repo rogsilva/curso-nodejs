31c2a60c349a073799769e52087dfb36c6ceacd8
Rogério Silva <ress.rogerio@gmail.com>
2016-12-07 22:19:33 -0200

    Refatoração utilizando controllers

---
+++ site_noticias/app/controllers/noticias.js
@@ -0,0 +1,43 @@
+module.exports.cadastrar = function (app, req, res) {
+  res.render('admin/form_add_noticia', {erros: {}, noticia: {}});
+}
+
+module.exports.listar = function (app, req, res) {
+  var connection = app.config.dbConnection();
+  var noticiasModel = new app.app.models.noticiasModel(connection);
+  noticiasModel.getNoticias(function(error, result){
+    res.render('noticias/noticias', {noticias : result});
+  });
+}
+
+module.exports.getId = function (app, req, res) {
+  var connection = app.config.dbConnection();
+  var noticiasModel = new app.app.models.noticiasModel(connection);
+  noticiasModel.getNoticia(function(error, result){
+    res.render('noticias/noticia', {noticia : result});
+  });
+}
+
+module.exports.salvar = function (app, req, res) {
+  var connection = app.config.dbConnection();
+  var noticiasModel = new app.app.models.noticiasModel(connection);
+  var noticia = req.body;
+
+  req.assert('titulo', 'O título é obrigatório').notEmpty();
+  req.assert('resumo', 'O Resumo é obrigatório').notEmpty();
+  req.assert('resumo', 'O Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+  req.assert('autor', 'O Autor é obrigatório').notEmpty();
+  req.assert('data_noticia', 'A Data é obrigatória').notEmpty().isDate({format: 'YYYY-MM-DD'});
+  req.assert('noticia', 'A notícia é obrigatória').notEmpty();
+
+  var erros = req.validationErrors();
+
+  if (erros) {
+    res.render('admin/form_add_noticia', {erros: erros, noticia: noticia});
+    return;
+  }
+
+  noticiasModel.salvarNoticia(noticia, function(error, result){
+    res.redirect("/noticias");
+  });
+}
--- site_noticias/app/routes/noticias.js
+++ site_noticias/app/routes/noticias.js
@@ -1,48 +1,19 @@
 //var dbConnection = require("../../config/dbConnection");
+module.exports = function(application) {
 
-module.exports = function(app) {
-
-  app.get('/cadastrar-noticia', function(req, res){
-    res.render('admin/form_add_noticia', {erros: {}, noticia: {}});
+  application.get('/cadastrar-noticia', function(req, res){
+    application.app.controllers.noticias.cadastrar(application, req, res);
   });
 
-  app.get('/noticias', function(req, res){
-    var connection = app.config.dbConnection();
-    var noticiasModel = new app.app.models.noticiasModel(connection);
-    noticiasModel.getNoticias(function(error, result){
-      res.render('noticias/noticias', {noticias : result});
-    });
+  application.get('/noticias', function(req, res){
+    application.app.controllers.noticias.listar(application, req, res);
   });
 
-  app.get('/noticia', function(req, res){
-    var connection = app.config.dbConnection();
-    var noticiasModel = new app.app.models.noticiasModel(connection);
-    noticiasModel.getNoticia(function(error, result){
-      res.render('noticias/noticia', {noticia : result});
-    });
+  application.get('/noticia', function(req, res){
+    application.app.controllers.noticias.getId(application, req, res);
   });
 
-  app.post('/noticias/salvar', function(req, res){
-    var connection = app.config.dbConnection();
-    var noticiasModel = new app.app.models.noticiasModel(connection);
-    var noticia = req.body;
-
-    req.assert('titulo', 'O título é obrigatório').notEmpty();
-    req.assert('resumo', 'O Resumo é obrigatório').notEmpty();
-    req.assert('resumo', 'O Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
-    req.assert('autor', 'O Autor é obrigatório').notEmpty();
-    req.assert('data_noticia', 'A Data é obrigatória').notEmpty().isDate({format: 'YYYY-MM-DD'});
-    req.assert('noticia', 'A notícia é obrigatória').notEmpty();
-
-    var erros = req.validationErrors();
-
-    if (erros) {
-      res.render('admin/form_add_noticia', {erros: erros, noticia: noticia});
-      return;
-    }
-
-    noticiasModel.salvarNoticia(noticia, function(error, result){
-      res.redirect("/noticias");
-    });
+  application.post('/noticias/salvar', function(req, res){
+    application.app.controllers.noticias.salvar(application, req, res);
   });
 }
